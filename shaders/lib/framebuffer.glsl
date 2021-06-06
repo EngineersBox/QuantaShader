6ba04bb3a563fa6bfe8cd9ede6e58e43ca964848
u6940136@anu.edu.au <toastcraft.info@gmail.com>
2021-06-06 22:59:44 +1000

    Added definitions for specular, shadow, depth and noise gl_FragData entries

--- shaders/lib/framebuffer.glsl
+++ shaders/lib/framebuffer.glsl
@@ -8,6 +8,14 @@ uniform sampler2D gdepth;
 #define GCOLOR_OUT gl_FragData[0]
 #define GDEPTH_OUT gl_FragData[1]
 #define GNORMAL_OUT gl_FragData[2]
+#define GSPECULAR_OUT gl_FragData[3]
+#define GSHADOW_TEX_0_OUT gl_FragData[4]
+#define GSHADOW_TEX_1_OUT gl_FragData[5]
+#define GDEPTH_TEX_0_OUT gl_FragData[6]
+#define GDEPTH_TEX_1_OUT gl_FragData[12]
+#define GSHADOW_COLOR_0_OUT gl_FragData[13]
+#define GSHADOW_COLOR_1_OUT gl_FragData[14]
+#define GNOISE_TEX_OUT gl_FragData[15]
 
 vec3 getAlbedo(in vec2 coord) {
     return texture2D(gcolor, coord).rgb;
